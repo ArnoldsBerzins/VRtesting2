5187cee59127dfe360df4824586ce1d2695acc5f
ArnoldsBerzins <78017323+ArnoldsBerzins@users.noreply.github.com>
2023-03-17 13:58:33 +0200

    progress i guess

---
+++ Assets/Scripts/RandomSpawnerSphere2.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomSpawnerSphere2 : MonoBehaviour
+{
+    public GameObject targetPrefab;
+    static int targetsHit;
+    public ChangeText changeTextScript;
+    public FireBulletOnActivate fireBulletOnActivateScript;
+    public MovingTargets script;
+    private void Start()
+    {
+        script = GetComponent<MovingTargets>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Bullet"))
+        {
+            Vector3 randomSpawnPosition = new Vector3(Random.Range(-4, 5), 3, -5);
+            Destroy(this.gameObject);
+            script.enabled = true;
+            Instantiate(targetPrefab, randomSpawnPosition, Quaternion.identity);
+            AddScore();
+            gameObject.SetActive(true);
+        }
+    }
+
+    public void AddScore()
+    {
+        targetsHit = targetsHit + 1;
+        changeTextScript.updateScore(targetsHit);
+        fireBulletOnActivateScript.CalculateAccuracySphere(targetsHit);
+    }
+}
